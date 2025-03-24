58c18c87584360c600c2f48ab172a2c6a0d3ec00
Gustavo Silva <gustavo.silva@altervision.com.br>
2025-03-24 09:49:43 -0300

    process with name

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: osinfo
+	./osinfo
+
+osinfo: osinfo.c
+	gcc osinfo.c -o osinfo
+
+
+clean:
+	rm -f osinfo
--- osinfo.c
+++ osinfo.c
@@ -6,27 +6,31 @@
 #include <ctype.h>
 #include <dirent.h>
 
-void get_version    (char *buffer, size_t size);
-void get_uptime     (char *buffer, size_t size);
-void get_downtime   (char *buffer, size_t size);
-void get_datetime   (char *buffer, size_t size);
-void get_cpuinfo    (char *buffer, size_t size);
-void get_loadavg    (char *buffer, size_t size);
-void get_io_stats   (char *buffer, size_t size);
-void get_filesystem (char *buffer, size_t size);
-void get_devices    (char *buffer, size_t size);
-void get_net_devices (char *buffer, size_t size);
+void get_version(char *buffer, size_t size);
+void get_uptime(char *buffer, size_t size);
+void get_downtime(char *buffer, size_t size);
+void get_datetime(char *buffer, size_t size);
+void get_cpuinfo(char *buffer, size_t size);
+void get_loadavg(char *buffer, size_t size);
+void get_io_stats(char *buffer, size_t size);
+void get_filesystem(char *buffer, size_t size);
+void get_devices(char *buffer, size_t size);
+void get_net_devices(char *buffer, size_t size);
 void get_process_list(char *buffer, size_t size);
 
-int main() {
-    while (1) {
+int main()
+{
+    while (1)
+    {
         FILE *file = fopen("index.html", "w");
-        if (!file) {
+        if (!file)
+        {
             perror("Can't open html file");
             return 1;
         }
 
-        if (flock(fileno(file), LOCK_EX) != 0) {
+        if (flock(fileno(file), LOCK_EX) != 0)
+        {
             perror("Can't block file");
             fclose(file);
             return 1;
@@ -35,7 +39,7 @@ int main() {
         fprintf(file, "<html><head><meta http-equiv=\"refresh\" content=\"3\"></head><body>\n");
         fprintf(file, "<h1>System Info</h1>\n");
 
-        char buffer[1024];
+        char buffer[2048];
 
         get_version(buffer, sizeof(buffer));
         fprintf(file, "%s\n", buffer);
@@ -66,8 +70,8 @@ int main() {
 
         get_net_devices(buffer, sizeof(buffer));
         fprintf(file, "%s", buffer);
-	
-	get_process_list(buffer, sizeof(buffer));
+
+        get_process_list(buffer, sizeof(buffer));
         fprintf(file, "%s", buffer);
 
         fprintf(file, "</body></html>\n");
@@ -75,137 +79,174 @@ int main() {
         flock(fileno(file), LOCK_UN);
         fclose(file);
 
-        sleep(3); 
+        sleep(3);
     }
 
     return 0;
 }
 
-void get_process_list(char *buffer, size_t size) {
-    DIR * dir;
+void get_process_list(char *buffer, size_t size)
+{
+    DIR *dir;
     struct dirent *entry;
     char temp[256];
-    char res[1024] = {0}; // Store process list
+    char res[2048] = {0}; // Store process list
 
     dir = opendir("/proc");
-    if (!dir) {
+    if (!dir)
+    {
         snprintf(buffer, size, "<p><strong>Process List:</strong> Unable to access /proc directory</p>\n");
         return;
     }
 
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL)
+    {
         // Check if the directory name consists only of digits (i.e., it's a process ID)
         int is_process = 1;
-        for (char *p = entry->d_name; *p; p++) {
-            if (!isdigit(*p)) {
+        for (char *p = entry->d_name; *p; p++)
+        {
+            if (!isdigit(*p))
+            {
                 is_process = 0;
                 break;
             }
         }
 
-        if (is_process) {
-            snprintf(temp, sizeof(temp), "PID: %s\n", entry->d_name);
-            strncat(res, temp, sizeof(res) - strlen(res) - 1);
+        if (is_process)
+        {
+            FILE *file;
+            char pid[32];
+            snprintf(pid, sizeof(pid), "/proc/%s/status", entry->d_name);
+            file = fopen(pid, "r");
+            if (!file)
+            {
+                snprintf(temp, sizeof(temp), "PID:%s-The comm file didn't open\n", entry->d_name);
+                strncat(res, temp, sizeof(res) - strlen(res) - 1);
+            }
+            else
+            {
+                char aux[64] = {0};
+                fgets(aux, sizeof(aux), file);
+                fclose(file);
+                snprintf(temp, sizeof(temp), "PID:%s- %s\n", entry->d_name, aux);
+                strncat(res, temp, sizeof(res) - strlen(res) - 1);
+
+            }
         }
     }
 
     closedir(dir);
 
-    snprintf(buffer, size, "<p><strong>Process List:</strong><br>%s</p>\n", res);
+    snprintf(buffer, size, "<p><strong>Process List:</strong><br>\n%s</p>\n", res);
 }
 
-void get_net_devices(char *buffer, size_t size) {
+void get_net_devices(char *buffer, size_t size)
+{
     FILE *file;
     char temp[32];
-    char res [1024] = {0};
+    char res[1024] = {0};
 
     file = fopen("/proc/net/dev", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Network devices:</strong> The file didn't open</p>\n");
         return;
     }
-    
-    while (fgets(temp, sizeof(temp), file)) {
-        // printf("%s", temp);
-        strncat(res, temp, sizeof(res)-strlen(res)-1);
-    }
-    fclose(file);    
 
-    snprintf(buffer, size, "<p><strong>Network devices:</strong> %s</p>\n", res);    
+    while (fgets(temp, sizeof(temp), file))
+    {
+        // printf("%s", temp);
+        strncat(res, temp, sizeof(res) - strlen(res) - 1);
+    }
+    fclose(file);
+
+    snprintf(buffer, size, "<p><strong>Network devices:</strong> %s</p>\n", res);
 }
 
-void get_devices(char *buffer, size_t size) {
+void get_devices(char *buffer, size_t size)
+{
     FILE *file;
     char temp[32];
-    char res [1024] = {0};
+    char res[1024] = {0};
 
     file = fopen("/proc/devices", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Devices:</strong> The file didn't open</p>\n");
         return;
     }
-    
-    while (fgets(temp, sizeof(temp), file)) {
+
+    while (fgets(temp, sizeof(temp), file))
+    {
         // printf("%s", temp);
-        strncat(res, temp, sizeof(res)-strlen(res)-1);
+        strncat(res, temp, sizeof(res) - strlen(res) - 1);
     }
     fclose(file);
 
     snprintf(buffer, size, "<p><strong>Devices:</strong> %s</p>\n", res);
 }
 
-void get_filesystem(char *buffer, size_t size) {
+void get_filesystem(char *buffer, size_t size)
+{
     FILE *file;
     char temp[32];
-    char res [1024] = {0};
+    char res[1024] = {0};
 
     file = fopen("/proc/filesystems", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Filesystem:</strong> The file didn't open</p>\n");
         return;
     }
-    
-    while (fgets(temp, sizeof(temp), file)) {
+
+    while (fgets(temp, sizeof(temp), file))
+    {
         // printf("%s", temp);
-        strncat(res, temp, sizeof(res)-strlen(res)-1);
+        strncat(res, temp, sizeof(res) - strlen(res) - 1);
     }
     fclose(file);
 
     snprintf(buffer, size, "<p><strong>Filesystem:</strong> %s</p>\n", res);
 }
 
-void get_io_stats (char *buffer, size_t size) {
+void get_io_stats(char *buffer, size_t size)
+{
     FILE *file;
     char temp[256];
-    char res [1024] = {0};
-
+    char res[1024] = {0};
 
     file = fopen("/proc/diskstats", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Input/Output opertaions:</strong> The file didn't open</p>\n");
         return;
     }
-    while(fgets(temp, sizeof(temp), file)) {
-        char * ptr = strtok(temp, " ");
-        char aux [32]; 
-        int i = 0;  
-        while (ptr) {
-            if (i==2) {
-                snprintf(aux,  sizeof(aux),"\nDevice Name: %s ",ptr);
-                strncat (res, aux, sizeof(res)-strlen(res)-1);
+    while (fgets(temp, sizeof(temp), file))
+    {
+        char *ptr = strtok(temp, " ");
+        char aux[32];
+        int i = 0;
+        while (ptr)
+        {
+            if (i == 2)
+            {
+                snprintf(aux, sizeof(aux), "\nDevice Name: %s ", ptr);
+                strncat(res, aux, sizeof(res) - strlen(res) - 1);
             }
-            else if (i==3) {
-                snprintf(aux, sizeof(aux),"- reads: %s ",ptr);
-                strncat (res, aux, sizeof(res)-strlen(res)-1);
+            else if (i == 3)
+            {
+                snprintf(aux, sizeof(aux), "- reads: %s ", ptr);
+                strncat(res, aux, sizeof(res) - strlen(res) - 1);
             }
-            else if (i==7) {
-                snprintf(aux, sizeof(aux),"- writes: %s\n",ptr);
-                strncat (res, aux, sizeof(res)-strlen(res)-1);
+            else if (i == 7)
+            {
+                snprintf(aux, sizeof(aux), "- writes: %s\n", ptr);
+                strncat(res, aux, sizeof(res) - strlen(res) - 1);
                 break;
             }
             ptr = strtok(0, " ");
             i++;
-	    }
+        }
     }
 
     fclose(file);
@@ -213,18 +254,20 @@ void get_io_stats (char *buffer, size_t size) {
     snprintf(buffer, sizeof(res), "<p><strong>Input/Output opertaions:</strong> %s</p>\n", res);
 }
 
-
-void get_version(char *buffer, size_t size) {
+void get_version(char *buffer, size_t size)
+{
     FILE *file;
     char temp[256];
 
     file = fopen("/proc/version", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>System version and Kernel:</strong> The file didn't open</p>\n");
         return;
     }
 
-    if (!fgets(temp, sizeof(temp), file)) {
+    if (!fgets(temp, sizeof(temp), file))
+    {
         snprintf(buffer, size, "<p><strong>System version and Kernel:</strong> Can't read file</p>\n");
         fclose(file);
         return;
@@ -234,18 +277,21 @@ void get_version(char *buffer, size_t size) {
     snprintf(buffer, size, "<p><strong>System version and Kernel:</strong> %s</p>\n", temp);
 }
 
-void get_uptime(char *buffer, size_t size) {
+void get_uptime(char *buffer, size_t size)
+{
     FILE *file;
     double uptime;
 
     file = fopen("/proc/uptime", "r");
 
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Uptime:</strong> The file didn't open</p>\n");
         return;
     }
 
-    if (fscanf(file, "%lf", &uptime) != 1) {
+    if (fscanf(file, "%lf", &uptime) != 1)
+    {
         snprintf(buffer, size, "<p><strong>Uptime:</strong> Can't read file</p>\n");
         fclose(file);
         return;
@@ -262,18 +308,21 @@ void get_uptime(char *buffer, size_t size) {
     snprintf(buffer, size, "<p><strong>Uptime:</strong> %02d:%02d:%02d:%02d</p>\n", days, hours, minutes, seconds);
 }
 
-void get_downtime(char *buffer, size_t size) {
+void get_downtime(char *buffer, size_t size)
+{
     FILE *file;
     double downtime;
 
     file = fopen("/proc/uptime", "r");
 
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>downtime:</strong> The file didn't open</p>\n");
         return;
     }
 
-    if (fscanf(file, "%*lf %lf", &downtime) != 1) {
+    if (fscanf(file, "%*lf %lf", &downtime) != 1)
+    {
         snprintf(buffer, size, "<p><strong>downtime:</strong> Can't read file</p>\n");
         fclose(file);
         return;
@@ -290,21 +339,26 @@ void get_downtime(char *buffer, size_t size) {
     snprintf(buffer, size, "<p><strong>downtime:</strong> %02d:%02d:%02d:%02d</p>\n", days, hours, minutes, seconds);
 }
 
-void get_datetime(char *buffer, size_t size) {
+void get_datetime(char *buffer, size_t size)
+{
     FILE *file;
     char line[128], time[16], date[16];
 
     file = fopen("/proc/driver/rtc", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>=DateTime:</strong> The file didn't open</p>\n");
         return;
     }
 
-    while (fgets(line, sizeof(line), file)) {
-        if (sscanf(line, "rtc_time        : %15s", time) == 1) {
+    while (fgets(line, sizeof(line), file))
+    {
+        if (sscanf(line, "rtc_time        : %15s", time) == 1)
+        {
             continue;
         }
-        if (sscanf(line, "rtc_date        : %15s", date) == 1) {
+        if (sscanf(line, "rtc_date        : %15s", date) == 1)
+        {
             break;
         }
     }
@@ -314,30 +368,37 @@ void get_datetime(char *buffer, size_t size) {
     snprintf(buffer, size, "<p><strong>DateTime:</strong> %s %s</p>\n", date, time);
 }
 
-void get_cpuinfo(char *buffer, size_t size) {
+void get_cpuinfo(char *buffer, size_t size)
+{
     FILE *file;
     char line[128], cpu[128] = "Unknown";
     double mhz = 0;
     int cores = 0;
 
     file = fopen("/proc/cpuinfo", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Processor Information:</strong> The file didn't open</p>\n");
         return;
     }
 
-    while (fgets(line, sizeof(line), file)) {
-        if (strncmp(line, "model name", 10) == 0) {
-            char *colon = strchr(line, ':'); 
-            if (colon) {
-                strncpy(cpu, colon + 2, sizeof(cpu) - 1); 
-                cpu[strcspn(cpu, "\n")] = '\0';  
+    while (fgets(line, sizeof(line), file))
+    {
+        if (strncmp(line, "model name", 10) == 0)
+        {
+            char *colon = strchr(line, ':');
+            if (colon)
+            {
+                strncpy(cpu, colon + 2, sizeof(cpu) - 1);
+                cpu[strcspn(cpu, "\n")] = '\0';
             }
-        } 
-        else if (sscanf(line, "cpu MHz         : %lf", &mhz) == 1) {
+        }
+        else if (sscanf(line, "cpu MHz         : %lf", &mhz) == 1)
+        {
             continue;
-        } 
-        else if (sscanf(line, "cpu cores       : %d", &cores) == 1) {
+        }
+        else if (sscanf(line, "cpu cores       : %d", &cores) == 1)
+        {
             break;
         }
     }
@@ -347,17 +408,20 @@ void get_cpuinfo(char *buffer, size_t size) {
     snprintf(buffer, size, "<p><strong>Processor Information:</strong> %s with %.2f MHz and %d core(s)</p>\n", cpu, mhz, cores);
 }
 
-void get_loadavg(char *buffer, size_t size) {
+void get_loadavg(char *buffer, size_t size)
+{
     FILE *file;
     double load1, load5, load15;
 
     file = fopen("/proc/loadavg", "r");
-    if (!file) {
+    if (!file)
+    {
         snprintf(buffer, size, "<p><strong>Load Average:</strong> The file didn't open</p>\n");
         return;
     }
 
-    if (fscanf(file, "%lf %lf %lf", &load1, &load5, &load15) != 3) {
+    if (fscanf(file, "%lf %lf %lf", &load1, &load5, &load15) != 3)
+    {
         snprintf(buffer, size, "<p><strong>Load Average:</strong> Can't read file</p>\n");
         fclose(file);
         return;
